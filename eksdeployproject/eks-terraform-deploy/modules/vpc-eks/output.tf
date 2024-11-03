a8a90c6213aab2815909ca6afa8aba100e308afa
femithecoder <femithecoder@gmail.com>
2024-11-03 15:37:59 +0000

    updated

---
+++ eksdeployproject/eks-terraform-deploy/modules/vpc-eks/output.tf
@@ -0,0 +1,18 @@
+output "main-region" {
+  value = var.main-region
+}
+output "project_name" {
+  value = var.project_name
+}
+output "vpc_id" {
+  value = aws_vpc.eks.id
+}
+output "public_subnet" {
+  value = aws_subnet.eks-public_subnet.id
+}
+output "private_subnet" {
+  value = aws_subnet.eks-private_subnet.id
+}
+output "internet_gateway" {
+  value = aws_internet_gateway.eks-igw
+}
---
+++ eksdeployproject/eks-terraform-deploy/modules/vpc-eks/variable.tf
@@ -0,0 +1,5 @@
+variable "main-region" {}
+variable "project_name" {}
+variable "vpc_cidr" {}
+variable "vpc_cidr_public_subnet" {}
+variable "vpc_cidr_private_subnet" {}
