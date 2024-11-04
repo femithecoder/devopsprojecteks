a6f1fb46a9706fb2dccbbd7facc5fdb675dfbe7f
femithecoder <femithecoder@gmail.com>
2024-11-04 21:42:24 +0000

    eksdeployment file cloned successfully

---
+++ eksdeployproject/devopsprojectt/eks-terraform-deploy/modules/eks-cluster/variables.tf
@@ -0,0 +1,35 @@
+################################################################################
+# General Variables from root module
+################################################################################
+variable "cluster_name" {
+  type    = string
+  default = "eks-cluster"
+}
+
+################################################################################
+# Variables from other Modules
+################################################################################
+
+variable "vpc_id" {
+  description = "VPC ID which EKS cluster is deployed in"
+  type        = string
+  default = "vpc-0d08fd8fcd58772d7"
+}
+
+variable "private_subnets" {
+  description = "VPC Private Subnets which EKS cluster is deployed in"
+  type        = list(any)
+  default = [ "subnet-0b011401e6a321e0f" ]
+}
+
+################################################################################
+# Variables defined using Environment Variables
+################################################################################
+
+# variable "rolearn" {
+#   description = "Add admin role to the aws-auth configmap"
+# }
+
+variable "main-region" {
+  default = "eu-west-2"
+}
