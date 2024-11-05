0215f24db0d1ee1f9b551f979233a6e5edae3d26
femithecoder <femithecoder@gmail.com>
2024-11-05 05:47:29 +0000

    updated

--- eksdeployproject/devopsprojectt/eks-terraform-deploy/scripts/update-kubeconfig.sh
+++ eksdeployproject/devopsprojectt/eks-terraform-deploy/scripts/update-kubeconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-response="$(aws eks list-clusters --region us-west-2 --output text | grep -i dominion-cluster 2>&1)" 
+response="$(aws eks list-clusters --region eu-west-2 --output text | grep -i eks-cluster 2>&1)" 
 if [[ $? -eq 0 ]]; then
-    echo "Success: Dominion-cluster exist"
-    aws eks --region us-west-2 update-kubeconfig --name dominion-cluster && export KUBE_CONFIG_PATH=~/.kube/config
+    echo "Success: eks-cluster exist"
+    aws eks --region eu-west-2 update-kubeconfig --name eks-cluster && export KUBE_CONFIG_PATH=~/.kube/config
 
 else
-    echo "Error: Dominion-cluster does not exist"
+    echo "Error: eks-cluster does not exist"
 fi
